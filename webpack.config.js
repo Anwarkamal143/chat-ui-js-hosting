4003ea242aae50e9ae9b87e9b20038a1c1b02133
Anwarkamal143 <anwarkamal1434@gmail.com>
2021-07-27 15:29:22 +0500

    change build directory

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   //   .sync("build/static/?(js|css)/*.?(js|css)")
   //   .map((f) => path.resolve(__dirname, f)),
   output: {
-    path: path.resolve(__dirname, 'lib'),
+    path: path.resolve(__dirname, 'build'),
     filename: "chatWidget.min.js",
     // libraryTarget: "commonjs2",
     library: "MyWidget",
