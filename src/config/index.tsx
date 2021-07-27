c5eb62480e196c1bc50397ae10f9d371742ac79c
Anwarkamal143 <anwarkamal1434@gmail.com>
2021-07-27 15:23:13 +0500

    initial boilerplate

---
+++ src/config/index.tsx
@@ -0,0 +1,4 @@
+export const ISCHAT_FEATURE = process.env.REACT_APP_CHAT === 'true'
+export const ISFAQ_FEATURE = process.env.REACT_APP_FAQ === 'true'
+export const ISTICKET_FEATURE = process.env.REACT_APP_TICKET === 'true'
+export const ISCALLBACK_FEATURE = process.env.REACT_APP_CALLBACK === 'true'
---
+++ webpack.npm.config.js
@@ -0,0 +1,122 @@
+const path = require("path");
+const glob = require("glob");
+const webpack = require("webpack");
+module.exports = {
+  //do stuff with the webpack config...
+  mode: "production",
+  entry: path.join(__dirname, "./src/embeddedWidget/widget.ts"),
+  // entry: glob
+  //   .sync("build/static/?(js|css)/*.?(js|css)")
+  //   .map((f) => path.resolve(__dirname, f)),
+  output: {
+    path: path.resolve(__dirname, "npmChatWidget"),
+    filename: "chatWidget.js",
+    // libraryTarget: "commonjs2",
+    library: "MyWidget",
+    libraryTarget: "umd",
+    umdNamedDefine: true,
+  },
+  // devtool: "source-map",
+  resolve: {
+    extensions: [
+      ".ts",
+      ".tsx",
+      ".js",
+      ".css",
+      ".txt",
+      ".svg",
+      ".png",
+      ".jpg",
+      ".jpeg",
+    ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        exclude: /node_modules/,
+        include: [path.resolve("src")],
+        loader: "ts-loader",
+        options: {
+          transpileOnly: false,
+          compilerOptions: {
+            module: "es2015",
+          },
+
+          configFile: require.resolve("./tsconfig-widget.json"),
+        },
+      },
+      {
+        test: /\.(scss|css)$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "resolve-url-loader",
+            // options: {...}
+          },
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+
+  plugins: [
+    new webpack.DefinePlugin({
+      process: {
+        env: {
+          REACT_APP_CHAT: "true",
+          REACT_APP_FAQ: "true",
+          REACT_APP_CALLBACK: "true",
+          REACT_APP_TICKET: "true",
+          NODE_ENV: JSON.stringify("production"),
+        },
+      },
+      // process: { env: { ...process.env } },
+    }),
+  ],
+};
+// const webpack = require("webpack");
+// const config = {
+//   entry: path.join(__dirname, "./src/index.ts"),
+//   output: {
+//     path: path.resolve(__dirname, "./docs"),
+//     filename: "simple-calendar.js",
+//     libraryTarget: "commonjs2",
+//   },
+//   devtool: "source-map",
+//   resolve: {
+//     extensions: [".ts", ".tsx", ".js", ".css", ".txt"],
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.ts(x?)$/,
+//         exclude: /node_modules/,
+//         include: [path.resolve("src")],
+//         loader: "ts-loader",
+//         options: {
+//           transpileOnly: false,
+//           compilerOptions: {
+//             module: "es2015",
+//           },
+//         },
+//       },
+//       {
+//         test: /\.css$/i,
+//         use: ["style-loader", "css-loader", "sass-loader"],
+//       },
+//     ],
+//   },
+//   // plugins: [
+//   //   new webpack.DefinePlugin({
+//   //     "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
+//   //     // process: { env: { ...process.env } },
+//   //     "process.env.REACT_APP_CHAT": JSON.stringify(process.env.REACT_APP_CHAT),
+//   //     "process.env.REACT_APP_FAQ": JSON.stringify(process.env.REACT_APP_FAQ),
+//   //     "process.env.REACT_APP_CALLBACK": JSON.stringify(
+//   //       process.env.REACT_APP_CALLBACK
+//   //     ),
+//   //   }),
+//   // ],
+// };
